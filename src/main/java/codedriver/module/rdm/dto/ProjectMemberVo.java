7c9c727a48653bc04efb84916c4b152c771e2182
lixs <lixs@techsure.com.cn>
2020-01-09 11:26:31 +0800

    补充注解

--- src/main/java/codedriver/module/rdm/dto/ProjectMemberVo.java
+++ src/main/java/codedriver/module/rdm/dto/ProjectMemberVo.java
@@ -1,18 +1,31 @@
 package codedriver.module.rdm.dto;
 
+import codedriver.framework.apiparam.core.ApiParamType;
+import codedriver.framework.common.dto.BasePageVo;
 import codedriver.framework.dto.UserVo;
+import codedriver.framework.restful.annotation.EntityField;
 
 /**
  * @program: codedriver
  * @description:
  * @create: 2019-12-23 18:25
  **/
-public class ProjectMemberVo {
+public class ProjectMemberVo extends BasePageVo {
+
+    @EntityField(name = "主键ID", type = ApiParamType.LONG)
     private Long id;
+
+    @EntityField(name = "项目Uuid", type = ApiParamType.STRING)
     private String projectUuid;
+
+    @EntityField(name = "用户id", type = ApiParamType.STRING)
     private String userId;
     private UserVo userVo;
+
+    @EntityField(name = "是否为组长", type = ApiParamType.INTEGER)
     private int isLeader;
+
+    @EntityField(name = "项目组id", type = ApiParamType.LONG)
     private Long groupId;
     private ProjectGroupVo groupVo;
 
