afd947e4de9ed3af403c883f362ddb22486d1cf1
chenqw <zenkiray@live.com>
2023-06-30 19:51:51 +0800

    - [功能]添加项目成员功能
    Signed-off-by: chenqw <zenkiray@live.com>

---
+++ src/main/java/neatlogic/module/rdm/api/project/SaveProjectUserApi.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright(c) 2023 NeatLogic Co., Ltd. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package neatlogic.module.rdm.api.project;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.auth.core.AuthAction;
+import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.rdm.auth.label.RDM_BASE;
+import neatlogic.framework.rdm.dto.ProjectUserVo;
+import neatlogic.framework.restful.annotation.Description;
+import neatlogic.framework.restful.annotation.Input;
+import neatlogic.framework.restful.annotation.OperationType;
+import neatlogic.framework.restful.annotation.Param;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
+import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
+import neatlogic.module.rdm.dao.mapper.ProjectMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+@Service
+@AuthAction(action = RDM_BASE.class)
+@OperationType(type = OperationTypeEnum.UPDATE)
+@Transactional
+public class SaveProjectUserApi extends PrivateApiComponentBase {
+    @Resource
+    private ProjectMapper projectMapper;
+
+    @Override
+    public String getName() {
+        return "nmrap.saveprojectuserapi.getname";
+    }
+
+    @Override
+    public String getConfig() {
+        return null;
+    }
+
+    @Input({@Param(name = "projectId", type = ApiParamType.LONG, desc = "term.rdm.projectid", isRequired = true),
+            @Param(name = "userType", type = ApiParamType.STRING, desc = "common.usertype", isRequired = true),
+            @Param(name = "userIdList", type = ApiParamType.JSONARRAY, desc = "common.userlist", isRequired = true)})
+    @Description(desc = "nmrap.saveprojectuserapi.getname")
+    @Override
+    public Object myDoService(JSONObject paramObj) {
+        JSONArray userIdList = paramObj.getJSONArray("userIdList");
+        String userType = paramObj.getString("userType");
+        Long projectId = paramObj.getLong("projectId");
+        for (int i = 0; i < userIdList.size(); i++) {
+            String userId = userIdList.getString(i).replace("user#", "");
+            ProjectUserVo projectUserVo = new ProjectUserVo();
+            projectUserVo.setUserId(userId);
+            projectUserVo.setProjectId(projectId);
+            projectUserVo.setUserType(userType);
+            projectMapper.insertProjectUser(projectUserVo);
+        }
+        return null;
+    }
+
+    @Override
+    public String getToken() {
+        return "/rdm/project/user/save";
+    }
+}
