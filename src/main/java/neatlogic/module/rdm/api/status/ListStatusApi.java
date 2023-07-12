c340f564c6565062a49efd81337692c09837b450
chenqw <zenkiray@live.com>
2023-07-12 14:47:54 +0800

    - [功能]状态流转权限校验
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/java/neatlogic/module/rdm/api/status/ListStatusApi.java
+++ src/main/java/neatlogic/module/rdm/api/status/ListStatusApi.java
@@ -16,7 +16,9 @@
 
 package neatlogic.module.rdm.api.status;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import neatlogic.framework.asynchronization.threadlocal.UserContext;
 import neatlogic.framework.auth.core.AuthAction;
 import neatlogic.framework.common.constvalue.ApiParamType;
 import neatlogic.framework.rdm.auth.label.RDM_BASE;
@@ -25,6 +27,7 @@ import neatlogic.framework.restful.annotation.*;
 import neatlogic.framework.restful.constvalue.OperationTypeEnum;
 import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import neatlogic.module.rdm.dao.mapper.AppMapper;
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -68,7 +71,38 @@ public class ListStatusApi extends PrivateApiComponentBase {
                 status = null;
             }
         }
-        List<AppStatusVo> statusList = appMapper.getStatusByAppId(paramObj.getLong("appId"), status);
+        List<AppStatusVo> statusList = appMapper.getStatusByAppId(appId, status);
+
+        for (int s = statusList.size() - 1; s >= 0; s--) {
+            AppStatusVo appStatus = statusList.get(s);
+            if (appStatus.getConfig() != null && CollectionUtils.isNotEmpty(appStatus.getConfig().getJSONArray("authList"))) {
+                boolean hasAuth = false;
+                JSONArray authList = appStatus.getConfig().getJSONArray("authList");
+                for (int i = 0; i < authList.size(); i++) {
+                    JSONObject authObj = authList.getJSONObject(i);
+                    String value = authObj.getString("value");
+                    if (value.startsWith("user#")) {
+                        if (UserContext.get().getAuthenticationInfoVo().getUserUuid().equalsIgnoreCase(value.replace("user#", ""))) {
+                            hasAuth = true;
+                            break;
+                        }
+                    } else if (value.startsWith("role#")) {
+                        if (UserContext.get().getAuthenticationInfoVo().getRoleUuidList().contains(value.replace("role#", ""))) {
+                            hasAuth = true;
+                            break;
+                        }
+                    } else if (value.startsWith("team#")) {
+                        if (UserContext.get().getAuthenticationInfoVo().getTeamUuidList().contains(value.replace("team#", ""))) {
+                            hasAuth = true;
+                            break;
+                        }
+                    }
+                }
+                if (!hasAuth) {
+                    statusList.remove(s);
+                }
+            }
+        }
         if (startStatus != null && !statusList.contains(startStatus)) {
             statusList.add(0, startStatus);
         }
