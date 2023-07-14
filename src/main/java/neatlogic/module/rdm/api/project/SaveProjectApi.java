2b40ca9bb2b7733a934e91ebc697fabd41e91aec
chenqw <zenkiray@live.com>
2023-07-14 22:19:50 +0800

    - [修复] 修复项目模板中的非系统属性导致创建项目失败问题
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/java/neatlogic/module/rdm/api/project/SaveProjectApi.java
+++ src/main/java/neatlogic/module/rdm/api/project/SaveProjectApi.java
@@ -118,7 +118,7 @@ public class SaveProjectApi extends PrivateApiComponentBase {
                             JSONObject attrObj = attrObjList.getJSONObject(i);
                             AppAttrVo appAttrVo = JSONObject.toJavaObject(attrObj, AppAttrVo.class);
                             //如果是内置属性检查是否还存在
-                            if (appAttrVo.getIsPrivate().equals(0) || (appAttrVo.getIsPrivate().equals(1) && AttrType.get(appAttrVo.getType()) != null)) {
+                            if (appAttrVo.getIsPrivate() == null || appAttrVo.getIsPrivate().equals(0) || (appAttrVo.getIsPrivate().equals(1) && AttrType.get(appAttrVo.getType()) != null)) {
                                 appAttrVo.setId(null);
                                 appAttrVo.setAppId(appVo.getId());
                                 appVo.addAppAttr(appAttrVo);
