2bb37b6a9ab0a32cbcf3db18221332f1e3220f30
chenqw <zenkiray@live.com>
2023-07-26 19:27:37 +0800

    - [修复] 补充接口参数说明
    Signed-off-by: chenqw <zenkiray@live.com>

--- src/main/java/neatlogic/module/rdm/api/appattr/SearchAttrApi.java
+++ src/main/java/neatlogic/module/rdm/api/appattr/SearchAttrApi.java
@@ -49,6 +49,7 @@ public class SearchAttrApi extends PrivateApiComponentBase {
     }
 
     @Input({@Param(name = "appId", desc = "nmraa.getappapi.input.param.desc", isRequired = true, type = ApiParamType.LONG),
+            @Param(name = "isActive", desc = "common.isactive", type = ApiParamType.INTEGER, rule = "0,1"),
             @Param(name = "needSystemAttr", desc = "nmraa.searchprivateattrapi.input.param.desc.needsystemattr", rule = "0,1", type = ApiParamType.INTEGER)})
     @Output({@Param(explode = AppAttrVo[].class)})
     @Description(desc = "nmraa.searchprivateattrapi.getname")
